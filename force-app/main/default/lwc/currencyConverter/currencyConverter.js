50e2d15cb07f8ee47afdf1a7eb983dcdef43a8c3
jayanth1947 <jayanthonteru111@gmail.com>
2023-04-17 15:23:58 +0530

    modified currencyConverter for static resources
    In this commit I have added the static resources and also
    imported the files inside the component and also applied styles

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,12 +1,15 @@
 import { LightningElement } from 'lwc';
 import {countryCodeList} from 'c/countryCodeList'
+import currencyConverterAssets from '@salesforce/resourceUrl/currencyConverterAssets';
+
 export default class CurrencyConverter extends LightningElement {
+    currencyImage=currencyConverterAssets+'/currencyConverterAssets/currency.svg'
     countryList=countryCodeList
 
     countryFrom="USD"
     countryTo="AUD"
 
-    
+
     handleChange(event){
         const {name,value}=event.target
         console.log("Name",name);
