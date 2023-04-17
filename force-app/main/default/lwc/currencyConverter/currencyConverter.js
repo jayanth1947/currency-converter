5334a3d87160ce21b6cba916ebfcbe147342be2f
jayanth1947 <jayanthonteru111@gmail.com>
2023-04-17 15:13:13 +0530

    Added the currencyCodeList
    In this commit I have added the combobox for the countryCodeList
    and also modified the styling of the dropdowns

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import {countryCodeList} from 'c/countryCodeList'
+export default class CurrencyConverter extends LightningElement {
+    countryList=countryCodeList
 
-export default class CurrencyConverter extends LightningElement {}
+    countryFrom="USD"
+    countryTo="AUD"
+
+    
+    handleChange(event){
+        const {name,value}=event.target
+        console.log("Name",name);
+        console.log("Value",value);
+        this[name]=value
+    }
+}
